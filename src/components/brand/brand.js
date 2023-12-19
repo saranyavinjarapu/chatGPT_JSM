0e2680a90719e1d456d35a99b7000d9415f34e96
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-12-19 18:13:25 +0530

    added brand component with necessary brand images and info

--- src/components/brand/brand.js
+++ src/components/brand/brand.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+import "./brand.css";
 
 const brand = () => {
-  return <div>brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google_brand" />
+      </div>
+      <div>
+        <img src={slack} alt="slack_brand" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian_brand" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox_brand" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify_brand" />
+      </div>
+    </div>
+  );
 };
 
 export default brand;
